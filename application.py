7b881d2b400ab9721efe17294c985f508d24177b
Tingkai911 <57085425+Tingkai911@users.noreply.github.com>
2020-06-07 17:00:36 +0800

    set secret key

--- application.py
+++ application.py
@@ -3,11 +3,11 @@ from flask_session import Session
 from tempfile import mkdtemp
 
 app = Flask(__name__)
-
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.secret_key = "secret_key" 
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
